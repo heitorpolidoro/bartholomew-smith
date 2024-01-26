f0745635880a8cdc8d49626a4da4502245877211
Heitor Polidoro <heitor.polidoro@gmail.com>
2024-01-26 16:54:04 -0300

    refactors

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import sys
 import markdown
 import sentry_sdk
 from flask import Flask, render_template
+from flask.cli import load_dotenv
 from githubapp import Config, webhook_handler
 from githubapp.events import (
     CheckSuiteCompletedEvent,
@@ -15,9 +16,9 @@ from githubapp.events import (
 )
 from githubapp.events.issues import IssueClosedEvent, IssuesEvent
 
-from src.managers.issue import handle_close_tasklist, handle_tasklist
-from src.managers.pull_request import handle_create_pull_request, handle_self_approver
-from src.managers.release import handle_release
+from src.managers.issue_manager import handle_close_tasklist, handle_tasklist
+from src.managers.pull_request_manager import handle_create_pull_request, handle_self_approver
+from src.managers.release_manager import handle_release
 
 logging.basicConfig(
     stream=sys.stdout,
@@ -49,6 +50,7 @@ sentry_init()
 webhook_handler.handle_with_flask(
     app, use_default_index=False, config_file=".bartholomew.yaml"
 )
+load_dotenv()
 Config.create_config("pull_request_manager", enabled=True, merge_method="SQUASH")
 Config.create_config("release_manager", enabled=True)
 Config.create_config("issue_manager", enabled=True)
@@ -100,8 +102,9 @@ def handle_check_suite_completed(event: CheckSuiteCompletedEvent):
      - Creates a Pull Request, if not exists, and/or enable the auto merge flag
     """
     if owner_pat := os.getenv("OWNER_PAT"):
+        repository = event.repository
         for pull_request in event.check_suite.pull_requests:
-            handle_self_approver(owner_pat, pull_request)
+            handle_self_approver(owner_pat,repository,  pull_request)
 
 
 @app.route("/", methods=["GET"])
