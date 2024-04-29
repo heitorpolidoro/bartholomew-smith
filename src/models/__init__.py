8eed36d2273d86733e3e3cb4bb017025697e5595
Heitor Polidoro <heitor.polidoro@gmail.com>
2024-04-29 15:30:30 -0300

    fix import

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,1 +1,9 @@
+from src.models.job import Job, JobStatus
+from src.models.issue_job import IssueJob, IssueJobStatus
 
+__all__ = [
+    "Job",
+    "JobStatus",
+    "IssueJob",
+    "IssueJobStatus",
+]
